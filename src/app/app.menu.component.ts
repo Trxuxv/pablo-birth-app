31ca86f05d894fb3fd1db965521162c78bc37eba
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-05-23 14:44:52 -0300

    menu toolbar

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -72,59 +72,6 @@ export class AppMenuComponent implements OnInit {
                     { label: 'Access Denied', icon: 'pi pi-fw pi-lock', routerLink: ['/access'] },
                     { label: 'Empty', icon: 'pi pi-fw pi-circle-off', routerLink: ['/pages/empty'] }
                 ]
-            },
-            {
-                label: 'Hierarchy', icon: 'pi pi-fw pi-align-left',
-                items: [
-                    {
-                        label: 'Submenu 1', icon: 'pi pi-fw pi-align-left',
-                        items: [
-                            {
-                                label: 'Submenu 1.1', icon: 'pi pi-fw pi-align-left',
-                                items: [
-                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-align-left' },
-                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-align-left' },
-                                    { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-align-left' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 1.2', icon: 'pi pi-fw pi-align-left',
-                                items: [
-                                    { label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-align-left' }
-                                ]
-                            },
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2', icon: 'pi pi-fw pi-align-left',
-                        items: [
-                            {
-                                label: 'Submenu 2.1', icon: 'pi pi-fw pi-align-left',
-                                items: [
-                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-align-left' },
-                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-align-left' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 2.2', icon: 'pi pi-fw pi-align-left',
-                                items: [
-                                    { label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-align-left' },
-                                ]
-                            },
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Start', icon: 'pi pi-fw pi-download',
-                items: [
-                    {
-                        label: 'Buy Now', icon: 'pi pi-fw pi-shopping-cart', url: ['https://www.primefaces.org/store']
-                    },
-                    {
-                        label: 'Documentation', icon: 'pi pi-fw pi-info-circle', routerLink: ['/documentation']
-                    }
-                ]
             }
         ];
     }
